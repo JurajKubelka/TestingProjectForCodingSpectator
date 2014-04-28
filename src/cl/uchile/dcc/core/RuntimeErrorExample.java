4f3a003102807eecd566d468d3b52aa3c07ec2ad
Juraj Kubelka <Juraj.Kubelka@gmail.com>
2014-04-28 18:27:18 -0400

    It adds runtime error example.
    Related to issue /karlitrotski/CodingSpectator/#2.
    Pair programming with @cinai.

---
+++ src/cl/uchile/dcc/core/RuntimeErrorExample.java
@@ -0,0 +1,14 @@
+package cl.uchile.dcc.core;
+
+public class RuntimeErrorExample {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int [] values = {1, 2, 3};
+		int value = values[4];
+		System.out.println(value);
+	}
+
+}
